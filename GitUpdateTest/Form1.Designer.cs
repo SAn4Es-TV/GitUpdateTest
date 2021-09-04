58a3febe0cc068b01a48ade36e3e0253d4966b60
SAn4Es_TV <sanes328@gmail.com>
2021-09-04 11:13:23 +0300

    First release

--- GitUpdateTest/Form1.Designer.cs
+++ GitUpdateTest/Form1.Designer.cs
@@ -34,6 +34,7 @@ namespace GitUpdateTest
             this.label2 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -66,7 +67,7 @@ namespace GitUpdateTest
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 81);
+            this.button2.Location = new System.Drawing.Point(12, 101);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(270, 23);
             this.button2.TabIndex = 3;
@@ -76,7 +77,7 @@ namespace GitUpdateTest
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(12, 52);
+            this.button3.Location = new System.Drawing.Point(12, 72);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(270, 23);
             this.button3.TabIndex = 4;
@@ -84,11 +85,21 @@ namespace GitUpdateTest
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 163);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
@@ -97,6 +108,7 @@ namespace GitUpdateTest
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -107,6 +119,7 @@ namespace GitUpdateTest
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- GitUpdateTest/Form1.cs
+++ GitUpdateTest/Form1.cs
@@ -17,9 +17,25 @@ namespace GitUpdateTest
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
-
+            await SolicenTEAM.Updater.CheckUpdate("SAn4Es-TV", "GitUpdateTest");
+            if(SolicenTEAM.Updater.UpdateVersion != SolicenTEAM.Updater.CurrentVersion && SolicenTEAM.Updater.UpdateVersion != "")
+            {
+                label3.Text = "New update awaliable!";
+                label2.Text = SolicenTEAM.Updater.UpdateDescription;
+                label1.Text = SolicenTEAM.Updater.UpdateVersion;
+                button2.Visible = true;
+                button3.Visible = true;
+            }
+            else
+            {
+                label3.Text = "Instaled the last version";
+                label2.Text = SolicenTEAM.Updater.UpdateDescription;
+                label1.Text = SolicenTEAM.Updater.UpdateVersion;
+                button2.Visible = false;
+                button3.Visible = false;
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -27,9 +43,13 @@ namespace GitUpdateTest
 
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private async void button3_Click(object sender, EventArgs e)
         {
-
+            await SolicenTEAM.Updater.CheckUpdate("SAn4Es-TV", "GitUpdateTest");
+            if (SolicenTEAM.Updater.UpdateVersion != SolicenTEAM.Updater.CurrentVersion && SolicenTEAM.Updater.UpdateVersion != "")
+            {
+                SolicenTEAM.Updater.ExtractArchive();
+            }
         }
     }
 }
